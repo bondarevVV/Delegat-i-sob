889615fce191c7c90ad8e230e9ff50f7d89a3147
bondarevVV <90005849+bondarevVV@users.noreply.github.com>
2022-04-18 16:34:34 +0300

    ,,,

---
+++ Delegat i sob/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace Delegat_i_sob
+{
+    internal class Timer
+    {
+        public void Start() 
+        {
+            for(int i = 0; i<99999; i++) 
+            {
+                Thread.Sleep(1000);
+                
+            }
+        }
+
+        public delegate void TimeChange(int sec);
+
+        public event TimeChange TimeChangeEvent;
+    }
+
+    internal class Clock
+    {
+        public void ParseTime(int sec) 
+        {
+            Console.WriteLine($"{sec} Секунда");
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Timer timer = new Timer();
+            Clock clock = new Clock();
+
+            timer.TimeChangeEvent += clock.ParseTime;
+
+            timer.Start();
+        }
+    }
+}
---
+++ Delegat i sob/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("Delegat i sob")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Delegat i sob")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("2c7a71af-b227-4c2c-9d66-12a511e4b02b")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
